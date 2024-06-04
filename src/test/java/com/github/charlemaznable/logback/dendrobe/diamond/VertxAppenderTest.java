aaa36eb04e749c35a48e2fee48522175748bdd9e
CharLemAznable <545541819@qq.com>
2024-06-04 17:01:07 +0800

    Fixed warning.

--- src/test/java/com/github/charlemaznable/logback/dendrobe/diamond/VertxAppenderTest.java
+++ src/test/java/com/github/charlemaznable/logback/dendrobe/diamond/VertxAppenderTest.java
@@ -54,8 +54,7 @@ public class VertxAppenderTest implements DiamondUpdaterListener, VertxManagerLi
         vertxOptions.setWorkerPoolSize(10);
         val hazelcastConfig = new Config();
         hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
-        vertxOptions.setClusterManager(new HazelcastClusterManager(hazelcastConfig));
-        vertx = VertxElf.buildVertx(vertxOptions);
+        vertx = VertxElf.buildVertx(vertxOptions, new HazelcastClusterManager(hazelcastConfig));
         vertx.eventBus().consumer("logback.diamond",
                 (Handler<Message<JsonObject>>) event -> {
                     try {
